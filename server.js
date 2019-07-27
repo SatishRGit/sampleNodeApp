8bc59107c4e9f42d2f72d254d8175242f6b12e64
Satish R <satishr@landmarkhealth.local>
2019-07-27 14:17:55 +0530

    make it better;

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+var http = require('http');
+
+const app = express();
+const port = 8080;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/sample1",(req,res)=>{
+	res.send("Hello mr Shade");
+});
+app.listen(port);
+
+
+//create a server object:
+/* http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080); */
